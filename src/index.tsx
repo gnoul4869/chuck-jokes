6c2d62083e4804d9a9581ed9838f8fe29e87a83d
gnoul_ <ttluong4869@gmail.com>
2022-12-16 20:13:46 +0700

    - Add assets folder - Add images

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-import './styles/index.scss';
+import './assets/styles/index.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
