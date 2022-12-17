6703ac5addda104c9043f5ab533ec1ce1ddf465a
gnoul_ <ttluong4869@gmail.com>
2022-12-17 18:32:24 +0700

    - Install React Router - Add pages - Update plan

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
