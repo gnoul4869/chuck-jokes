e2c9f841d303ccc5eaf7aff7a26789cb9bdc7ab1
gnoul_ <ttluong4869@gmail.com>
2022-12-18 22:59:55 +0700

    - Add JokeList component - Update Icon styling - Add arrow_down icon

--- src/components/JokeList/JokeList.tsx
+++ src/components/JokeList/JokeList.tsx
@@ -3,6 +3,7 @@ import useFetchData from 'hooks/useFetchData';
 import JokeCard from 'components/JokeCard/JokeCard';
 
 import './JokeList.scss';
+import Icon from 'components/Icon/Icon';
 
 export default function JokeList() {
     const [total, setTotal] = useState(6);
@@ -23,6 +24,10 @@ export default function JokeList() {
                     return <JokeCard key={joke.id} joke={joke} />;
                 })}
             </div>
+            <button className="btn-view-more" onClick={() => setTotal(total + 3)}>
+                <span className="text">View more</span>
+                <Icon name="arrow_down" size="sm" className="icon down" />
+            </button>
         </div>
     );
 }
