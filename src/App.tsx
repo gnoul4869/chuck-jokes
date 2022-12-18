34fe41d4ffe14e1c920db4ddbb9245db3fa9f412
gnoul_ <ttluong4869@gmail.com>
2022-12-18 18:20:04 +0700

    Add container

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,14 @@ import NotFound from 'pages/NotFound';
 export default function App() {
     return (
         <GlobalProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Index />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </BrowserRouter>
+            <div className="container">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Index />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
         </GlobalProvider>
     );
 }
